ec50bc77e1f0aca74a2b9e5a7fa6c40430b182db
Sohrab Saran <sohrabsaran@gmail.com>
2020-07-20 21:44:04 +0530

    Update loc.bash

--- codeQuality/loc.bash
+++ codeQuality/loc.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-find $1 -path $1/.git -prune -o -type f -exec sh -c '
+find $1 -type d \( -name node_modules -o -name .git -o -path name \) -prune -false -o -type f -exec sh -c '
   wc -l "$0"
 ' {} \;
