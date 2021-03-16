3ee85db5a9d0ba57b092bc823974b26d58bf7f02
Sohrab Saran <sohrabsaran@gmail.com>
2021-03-16 22:22:58 +0530

    Update script.mjs

--- frontend/filesystem/script.mjs
+++ frontend/filesystem/script.mjs
@@ -16,24 +16,24 @@
  */
 
 /*
-Below has started giving CORS issues (that were not there before?)
+Below has started giving CORS issues (that were not there before?)*/
 import {
   fileOpen,
   directoryOpen,
   fileSave,
   imageToBlob,
 } from 'https://unpkg.com/browser-nativefs'
-*/
+/**/
 
 let fileStructure
 
 (async () => {
-  const x = await loadScriptFromTextAtUrl('https://unpkg.com/browser-nativefs','module'); 
+  /*const x = await loadScriptFromTextAtUrl('https://unpkg.com/browser-nativefs','module'); 
   const fileOpen = x.fileOpen;
   const directoryOpen = x.directoryOpen;
   const fileSave = x.fileSave;
   const imageToBlob = x.imageToBlob;
-  
+  */
   const openButton = document.querySelector('#open');
   const openMultipleButton = document.querySelector('#open-multiple');
   const openDirectoryButton = document.querySelector('#open-directory');
