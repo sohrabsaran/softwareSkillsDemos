ecb35e90b00976ca5abc92f95fc882e64d615923
Sohrab Saran <sohrabsaran@gmail.com>
2023-02-22 13:48:22 +0530

    Create theatrePartnerMenu.js

---
+++ backend/programming/java/springBootDemoSupport/theatrePartnerMenu.js
@@ -0,0 +1,9 @@
+// Create the new node to insert
+const newNode = document.createElement("span");
+newNode.innerHTML = "TODO: from theatrePartnerMenu.js"
+
+// Get a reference to the parent node
+const parentNode = document.body;
+
+let firstElementOfBody = body.firstElementChild;
+parentDiv.insertBefore(newNode, firstElementOfBody);
