9bdd17d072b321491f35c19fffd4af92f4b42bcc
Sohrab Saran <sohrabsaran@gmail.com>
2023-02-22 14:44:05 +0530

    Update theatrePartnerMenu.js

--- backend/programming/java/springBootDemoSupport/theatrePartnerMenu.js
+++ backend/programming/java/springBootDemoSupport/theatrePartnerMenu.js
@@ -3,7 +3,9 @@ window.addEventListener('DOMContentLoaded', init, false);
 function init() {
 // Create the new node to insert
 const newNode = document.createElement("span");
-newNode.innerHTML = "TODO: from theatrePartnerMenu.js"
+newNode.innerHTML = /*html*/`
+<a href="">New Movie Schedule</a> <a href="">Scheduled Movies</a>
+`
 
 // Get a reference to the parent node
 const parentNode = document.body;
