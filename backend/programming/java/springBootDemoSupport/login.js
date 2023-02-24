048bbc9372b93a7b4f5b4a8b916ca26ed021c195
Sohrab Saran <sohrabsaran@gmail.com>
2023-02-24 17:53:23 +0530

    Update login.js

--- backend/programming/java/springBootDemoSupport/login.js
+++ backend/programming/java/springBootDemoSupport/login.js
@@ -1,10 +1,16 @@
-export function initLoginScript(passwordKey, buttonId, homePageRelativeUrl) {
+export function initLoginScript(passwordKey_, buttonId_, homePageRelativeUrl_) {
+  passwordKey = passwordKey_;
+  buttonId = buttonId_;
+  homePageRelativeUrl = homePageRelativeUrl_;
   //Ensure that on login page, we are not already logged in!
   //TODO: use JWT token instead of storing the password.
   localStorage.removeItem(passwordKey);
   const button = document.getElementById(buttonId);
   button.addEventListener("click", onLoginButtonClicked);
 }
+let passwordKey;
+let buttonId;
+let homePageRelativeUrl;
 
 function onLoginButtonClicked(){
     //TODO: call the login endpoint
